5f051dc2ac33b9219a96e9fcb56a3cabdf0e6bdd
Ahmad Abotrab <abotrab.ahmad3099@gmail.com>
2022-12-23 19:14:18 +0300

    client & server

---
+++ client and sever/server.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((request, response) => {
+
+    //  test send basic text and tag html
+
+    // response.setHeader('content-type','text/html');
+    // response.write('<h1>Welcome</h1>');
+    // response.write('<p>Hello ahmad</p>');
+    // response.end();
+
+
+    /// send full html page
+
+    // fs.readFile('./views/index.html', (err, data) => {
+    //     if (err) {
+    //         response.setHeader('content-type', 'text/plain');
+    //         response.write(err);
+    //         response.end();
+    //     } else {
+    //         response.setHeader('content-type', 'text/html');
+    //         response.write(data);
+    //         response.end();
+    //     }
+    // });
+
+
+    // Basic Route
+    response.setHeader('content-type', 'text/html');
+    let path = './views/';
+    switch (request.url) {
+        case '/': {
+            path += 'index.html';
+            break;
+        }
+        case '/about': {
+            path += 'about.html';
+            break;
+        }
+        default: {
+            path += '404.html';
+            response.statusCode = 404;
+            break;
+        }
+    }
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log(err);
+            response.end();
+        }
+        response.write(data);
+        response.end();
+    });
+});
+
+server.listen((3000), 'localhost', () => {
+    console.log('listen 3000');
+})
